2cdbd9f79e72754fb711c271f1ef1b67d3855ae4
XcXerxes <xcxerxes@aliyun.com>
2018-07-30 01:31:35 +0800

    add

---
+++ src/Button-arrow.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ButtonArrow extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+ButtonArrow.propTypes = {
+  position: PropTypes.string.isRequired
+}
+
+export default ButtonArrow
